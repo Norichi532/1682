9d0aa359bba3c8b8d5f9478170ff295e60d1c6de
Tan Phat <impphatdn99@gmail.com>
2026-03-29 22:51:43 +0700

    remove node_modules completely

--- backend/src/modules/auth/index.js
+++
@@ -1,9 +0,0 @@
-const User = require('../user/user.model');
-const authController = require('./auth.controller');
-const authRoutes = require('./auth.routes');
-
-module.exports = {
-  User,
-  authController,
-  authRoutes,
-};
